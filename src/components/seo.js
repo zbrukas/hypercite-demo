f3ce75059c4c2771ca7020e07b707c73d339c403
Heber Nobre <heber@thedreamers.us>
2023-11-13 15:17:11 -0500

    chore(dependencies): gatsby packages bump

--- src/components/seo.js
+++ src/components/seo.js
@@ -6,10 +6,9 @@
  */
 
 import * as React from "react"
-import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
-function Seo({ description, title, children }) {
+function Seo({ description = '', title, children }) {
   const { site } = useStaticQuery(
     graphql`
       query {
@@ -41,15 +40,6 @@ function Seo({ description, title, children }) {
       {children}
     </>
   )
-}
-
-Seo.defaultProps = {
-  description: ``,
-}
-
-Seo.propTypes = {
-  description: PropTypes.string,
-  title: PropTypes.string.isRequired,
 }
 
 export default Seo
